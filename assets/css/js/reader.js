95bbe3bd65f23d89c5d3d28cc35beb14126aed07
Yudha <komiku99u@gmail.com>
2026-06-26 21:51:35 +0700

    Create reader.js

---
+++ assets/css/js/reader.js
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0">
+
+    <title>ComicReader</title>
+
+    <link
+        rel="stylesheet"
+        href="assets/css/reader.css">
+
+</head>
+
+<body>
+
+    <header id="topbar">
+
+        <button id="backButton">
+            ← Library
+        </button>
+
+        <span id="comicTitle">
+            Loading...
+        </span>
+
+    </header>
+
+    <main id="reader">
+
+        <div id="loading">
+
+            Loading Comic...
+
+        </div>
+
+    </main>
+
+    <script src="assets/js/jszip.min.js"></script>
+
+    <script src="assets/js/api.js"></script>
+
+    <script src="assets/js/reader.js"></script>
+
+</body>
+
+</html>
