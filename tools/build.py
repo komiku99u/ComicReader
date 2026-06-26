aecccb8df6bd8e4b2f008c4c21e63e513019e03b
Yudha <komiku99u@gmail.com>
2026-06-26 21:58:31 +0700

    Add files via upload

---
+++ tools/build.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from core.runner import BuildRunner
+
+
+def main():
+
+    runner = BuildRunner()
+
+    runner.run()
+
+
+if __name__ == "__main__":
+    main()
